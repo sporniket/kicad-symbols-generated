130e427fd285f78aa78236e23afa083ac4cbb815
David SPORN <sporniket.studio@gmail.com>
2021-12-08 07:03:16 +0100

    generate a 'socket' symbol to each generated lib

--- ics/commons-make.mk
+++ ics/commons-make.mk
@@ -19,5 +19,10 @@ clean:
 %.lib:NAME_JSON ::= $(SRC_NAME).json
 %.lib:NAME_ITEM ::= $(SRC_NAME).symbol
 %.lib:NAME_ITEM_PHY ::= $(SRC_NAME).phy.symbol
+%.lib:NAME_ITEM_SOCKET ::= $(SRC_NAME).socket.symbol
 %.lib:%.md
-	$(TOOL_PATH)/jsonFromDatasheet.py $< tmp/$(NAME_JSON) && $(TOOL_PATH)/libItemFromJsonDatasheet.py tmp/$(NAME_JSON) tmp/$(NAME_ITEM) && $(TOOL_PATH)/libItemPhyFromJsonDatasheet.py tmp/$(NAME_JSON) tmp/$(NAME_ITEM_PHY) && $(TOOL_PATH)/compileSymbols.py tmp/$(NAME_ITEM) tmp/$(NAME_ITEM_PHY) $@
+	$(TOOL_PATH)/jsonFromDatasheet.py $< tmp/$(NAME_JSON) \
+	&& $(TOOL_PATH)/libItemFromJsonDatasheet.py tmp/$(NAME_JSON) tmp/$(NAME_ITEM) \
+	&& $(TOOL_PATH)/libItemPhyFromJsonDatasheet.py tmp/$(NAME_JSON) tmp/$(NAME_ITEM_PHY) \
+	&& $(TOOL_PATH)/libItemPhyFromJsonDatasheet.py --passive tmp/$(NAME_JSON) tmp/$(NAME_ITEM_SOCKET) \
+	&& $(TOOL_PATH)/compileSymbols.py tmp/$(NAME_ITEM) tmp/$(NAME_ITEM_PHY) tmp/$(NAME_ITEM_SOCKET) $@
